73ec84436496b9dae545ca3817f82e57d436826a
meherab nahin <nahinrahman87@gmail.com>
2025-10-20 18:21:40 +0600

    pushpush

---
+++ S_pushpush.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  
+  int n;cin>>n;
+  deque<int>dq;
+  for(int i=0;i<n;i++){
+    int x;cin>>x;
+    if(i%2==0){
+        dq.push_back(x);
+    }else{
+        dq.push_front(x);
+    }
+  } 
+  if(n%2!=0)reverse(dq.begin(),dq.end());
+  for(int i=0;i<n;i++){
+    cout<<dq[i]<<" ";
+  }
+  cout<<endl;
+  return 0;
+}

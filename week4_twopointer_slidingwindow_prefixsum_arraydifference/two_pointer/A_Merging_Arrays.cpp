a105c25c617e68adb15eee5e82e367e71acac221
meherab nahin <nahinrahman87@gmail.com>
2025-11-04 19:51:09 +0600

    merging arrays

---
+++ week4_twopointer_slidingwindow_prefixsum_arraydifference/two_pointer/A_Merging_Arrays.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  
+    int n,m;
+    cin>>n>>m;
+
+    vector<int>a(n),b(m);
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(int i=0;i<m;i++){
+        cin>>b[i];
+    }
+
+    vector<int>ans;
+    int i=0,j=0;
+
+    while(i<n && j<m){
+        if(a[i]<b[j]){
+            ans.push_back(a[i]);
+            i++;
+        }else if(a[i]==b[j]){
+            ans.push_back(a[i]);
+            ans.push_back(b[j]);
+            i++,j++;
+        }else{
+            ans.push_back(b[j]);
+            j++;
+        }
+    }
+
+    while(i<n){
+        ans.push_back(a[i]);
+        i++;
+    }
+
+    while(j<m){
+        ans.push_back(b[j]);
+        j++;
+    }
+
+    for(auto val:ans){
+        cout<<val<<" ";
+    }
+
+
+  return 0;
+}

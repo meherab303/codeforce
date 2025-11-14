841aa25ab072e81f8102158a603b206556a387fa
meherab nahin <nahinrahman87@gmail.com>
2025-11-14 09:24:12 +0600

    update number of segment with small sum

--- week5_variable_size_slidingwindow/variable_size_sliding_window/C_Number_of_Segments_with_Small_Sum.cpp
+++ week5_variable_size_slidingwindow/variable_size_sliding_window/C_Number_of_Segments_with_Small_Sum.cpp
@@ -27,12 +27,16 @@ int main()
         r++;
     }else{
 
-        while(sum>k){   // jotokkhon na sum k er shoman ba chuto hocche tokkhn left contribution remove korte hbe jeno ektao valid good segment baad na pore.
+        while(sum>k && l<=r){   // jotokkhon na sum k er shoman ba chuto hocche (obosshoi r range er modde .r ke cross kora jabena) tokkhn left contribution remove korte hbe jeno ektao valid good segment baad na pore.
              sum-=v[l];
              l++;
         }
-        good_seg+=r-l+1; // jokhon sum k er shoman ba chuto hbe tokkhn e oi good subarray  add korte hbe.
+        if(sum<=k){  // emon o hote pare l  r porjnto ashche but sum k theke still boroi ache
+             good_seg+=r-l+1; // jokhon sum k er shoman ba chuto hbe tokkhn e oi good subarray  add korte hbe.     
+        }
+
         r++;
+       
     }
   }
 
